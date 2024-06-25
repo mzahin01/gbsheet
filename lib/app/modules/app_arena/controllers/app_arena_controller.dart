19abcced9168b5af9ded938e20bef959eb6a3855
mzahin01 <minhazahin@gmail.com>
2024-06-26 02:07:20 +0600

    whatever...

--- lib/app/modules/app_arena/controllers/app_arena_controller.dart
+++ lib/app/modules/app_arena/controllers/app_arena_controller.dart
@@ -1,3 +1,46 @@
-import 'package:get/get.dart';
+// ignore_for_file: avoid_print
 
-class AppArenaController extends GetxController {}
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:rive/rive.dart';
+
+class AppArenaController extends GetxController {
+  late Artboard artboard;
+  StateMachineController? stateMachineController;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadRiveFile();
+  }
+
+  void loadRiveFile() async {
+    final data = await rootBundle.load('rive_assets/trial.riv');
+    final file = RiveFile.import(data);
+    artboard = file.mainArtboard;
+    stateMachineController = StateMachineController.fromArtboard(
+      artboard,
+      'State Machine 1',
+    );
+    if (stateMachineController != null) {
+      artboard.addController(stateMachineController!);
+    } else {
+      print('State Machine 1 not found in the Rive file');
+    }
+    update(); // Call update to refresh UI after loading the Rive file
+  }
+
+  void trigger1() {
+    stateMachineController?.findInput<bool>('Trigger 1')?.value = true;
+  }
+
+  void trigger2() {
+    stateMachineController?.findInput<bool>('Trigger 2')?.value = true;
+  }
+
+  void trigger3() {
+    stateMachineController?.findInput<bool>('Trigger 3')?.value = true;
+  }
+
+  // Add methods for other triggers as needed
+}
--- lib/app/modules/app_arena/views/app_arena_view.dart
+++ lib/app/modules/app_arena/views/app_arena_view.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:rive/rive.dart';
 import '../controllers/app_arena_controller.dart';
 
 class AppArenaView extends GetView<AppArenaController> {
   const AppArenaView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('AppArenaView'),
-          centerTitle: true,
-        ),
-        body: const Stack(
-          children: [],
-        ));
+      appBar: AppBar(title: const Text('Rive Animation with GetX')),
+      body: GetBuilder<AppArenaController>(
+        init: AppArenaController(),
+        builder: (controller) {
+          return Column(
+            children: [
+              Expanded(
+                child: Rive(artboard: controller.artboard),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => controller.trigger1(),
+                    child: const Text('Trigger 1'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => controller.trigger2(),
+                    child: const Text('Trigger 2'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => controller.trigger3(),
+                    child: const Text('Trigger 3'),
+                  ),
+                  // Add buttons for other triggers as needed
+                ],
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
