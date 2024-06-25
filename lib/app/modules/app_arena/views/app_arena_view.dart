594aa90a855bfe4fb2a675686e81552437d2ed26
mzahin01 <minhazahin@gmail.com>
2024-06-26 03:19:41 +0600

    somehow listener still not working

--- lib/app/modules/app_arena/views/app_arena_view.dart
+++ lib/app/modules/app_arena/views/app_arena_view.dart
@@ -16,34 +16,31 @@ class AppArenaView extends GetView<AppArenaController> {
           if (controller.artboard == null) {
             return const Center(child: CircularProgressIndicator());
           } else {
-            return GestureDetector(
-              onTap: () => controller.onStarClicked(),
-              child: Container(
-                color: Colors.transparent,
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: Rive(artboard: controller.artboard!),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () => controller.trigger1(),
-                          child: const Text('Trigger 1'),
-                        ),
-                        ElevatedButton(
-                          onPressed: () => controller.trigger2(),
-                          child: const Text('Trigger 2'),
-                        ),
-                        ElevatedButton(
-                          onPressed: () => controller.trigger3(),
-                          child: const Text('Trigger 3'),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+            return Container(
+              color: Colors.transparent,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Rive(artboard: controller.artboard!),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () => controller.trigger1(),
+                        child: const Text('Trigger 1'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () => controller.trigger2(),
+                        child: const Text('Trigger 2'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () => controller.trigger3(),
+                        child: const Text('Trigger 3'),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             );
           }
