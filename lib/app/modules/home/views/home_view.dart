617215af7892201c4b58c35d4ce5c0c07fc3fb52
mzahin01 <minhazahin@gmail.com>
2024-06-27 17:05:51 +0600

    changed home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -15,54 +15,72 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Row(
           children: [
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(Routes.WEB_PAGE_ARENA);
-                },
-                child: const Text('WebpageArena')),
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(Routes.APP_ARENA);
-                },
-                child: const Text('AppArena1')),
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(Routes.MACOS_ARENA);
-                },
-                child: const Text('MacOSArena')),
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(Routes.APP2);
-                },
-                child: const Text('AppArena2')),
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(Routes.KAJ_KORE);
-                },
-                child: const Text('Kaj Kore')),
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(Routes.SKILL_MACHINE);
-                },
-                child: const Text('Skill Machine')),
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(Routes.SKILL_MACHINE_GETX);
-                },
-                child: const Text('Skill Machine Getx')),
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(Routes.COPY_BILAI_COPY);
-                },
-                child: const Text('Woking Star')),
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(Routes.ANOTHER_PAGE);
-                },
-                child: const Text('Another Page')),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(Routes.SKILL_MACHINE);
+                  },
+                  child: const Text('Skill'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(Routes.SKILL_MACHINE_GETX);
+                  },
+                  child: const Text('Skill Getx'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(Routes.COPY_BILAI_COPY);
+                  },
+                  child: const Text('Woking Star'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(Routes.ANOTHER_PAGE);
+                  },
+                  child: const Text('Another Page'),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(Routes.WEB_PAGE_ARENA);
+                  },
+                  child: const Text('WebpageArena'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(Routes.APP_ARENA);
+                  },
+                  child: const Text('AppArena1'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(Routes.MACOS_ARENA);
+                  },
+                  child: const Text('MacOSArena'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(Routes.APP2);
+                  },
+                  child: const Text('AppArena2'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(Routes.KAJ_KORE);
+                  },
+                  child: const Text('Kaj Kore'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
